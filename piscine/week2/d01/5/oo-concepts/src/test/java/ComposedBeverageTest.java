2eba35b0fa71b56cca2ac839c3eed99db3606c2a
manar13297 <manarbadre13@gamil.com>
2024-02-20 15:58:46 +0100

    add a test unit to refactoring ex 5

---
+++ piscine/week2/d01/5/oo-concepts/src/test/java/ComposedBeverageTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComposedBeverageTest {
+
+    @Test
+    void ComputeCoffeWithMilkAndCreamPrice(){
+        var coffe = new BaseBeverage("COFFE", Main.COFFE_PRICE);
+        var milk = new BeverageSupplement("Milk", Main.MILK_PRICE);
+        var cream = new BeverageSupplement("Cream", Main.CREAM_PRICE);
+
+        var coffeWithMilkAndCream = new ComposedBeverage(coffe , Arrays.asList(milk, cream));
+        assertEquals(1.45 ,coffeWithMilkAndCream.price());
+    }
+
+}
