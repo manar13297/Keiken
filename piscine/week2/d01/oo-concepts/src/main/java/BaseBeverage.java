c5cc66a3301df6056b7931fdc9c2962a62b487c3
manar13297 <manarbadre13@gamil.com>
2024-02-20 15:09:19 +0100

    week 2 d01

---
+++ piscine/week2/d01/oo-concepts/src/main/java/BaseBeverage.java
@@ -0,0 +1,6 @@
+public class BaseBeverage extends BeverageComponent {
+
+    public BaseBeverage(String name, double price) {
+        super(name, price);
+    }
+}
---
+++ piscine/week2/d01/oo-concepts/src/main/java/Beverage.java
@@ -0,0 +1,4 @@
+public interface Beverage {
+    double price();
+    String name();
+}
